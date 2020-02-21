30d4d3d64d823334eed4837dff77c2c8aff849ab
apurva kulkarni <apurva165@gmail.com>
2020-02-21 07:21:52 -0600

    java 11 http client tutorial

---
+++ src/net/branchandbound/linkvalidator/LinkValidatorSync.java
@@ -0,0 +1,45 @@
+package net.branchandbound.linkvalidator;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class LinkValidatorSync {
+
+    private static HttpClient client;
+
+    public static void main(String[] args) throws IOException {
+        client = HttpClient.newHttpClient();
+
+        Files.lines(Path.of("urls.txt"))
+                .map(LinkValidatorSync::linkValidator)
+                .forEach(System.out::println);
+    }
+
+    private static String linkValidator(String link) {
+        HttpRequest request = HttpRequest.newBuilder(URI.create(link))
+                .GET()
+                .build();
+        try {
+            HttpResponse<Void> response = client.send(request, HttpResponse.BodyHandlers.discarding());
+            return responseToString(response);
+        } catch (InterruptedException | IOException exception) {
+            return String.format("", link, false);
+        }
+
+    }
+
+    private static String validateLink(String link) {
+        return null;
+    }
+
+    private static String responseToString(HttpResponse<Void> response) {
+        int status = response.statusCode();
+        boolean success = status >= 200 && status <= 299;
+        return String.format("%s -> %s (status: %s)", response.uri(), success, status);
+    }
+}
---
+++ src/net/branchandbound/linkvalidator/WebSocketExample.java
@@ -0,0 +1,51 @@
+package net.branchandbound.linkvalidator;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.WebSocket;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.CountDownLatch;
+
+public class WebSocketExample {
+    public static void main(String[] args) throws InterruptedException {
+        int msgcount = 5;
+        CountDownLatch recieveLatch = new CountDownLatch(msgcount);
+
+        CompletableFuture<WebSocket> weFuture = HttpClient.newHttpClient()
+                .newWebSocketBuilder()
+                .connectTimeout(Duration.ofSeconds(3))
+                .buildAsync(URI.create("wss://echo.websocket.org"), new EchoListener(recieveLatch));
+
+        weFuture.thenAccept(webSocket -> {
+            webSocket.request(msgcount);
+            for (int i = 0 ; i < msgcount ; i++){
+                webSocket.sendText("Message : " + i , true);
+            }
+        });
+
+        recieveLatch.await();
+    }
+
+    static class EchoListener implements WebSocket.Listener{
+
+        CountDownLatch recieveLatch;
+
+        public EchoListener(CountDownLatch recieveLatch) {
+            this.recieveLatch = recieveLatch;
+        }
+
+        @Override
+        public void onOpen(WebSocket webSocket) {
+            System.out.println("websocket open");
+        }
+
+        @Override
+        public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+            System.out.println("on Text :" + data);
+            recieveLatch.countDown();
+            return null;
+        }
+    }
+}
